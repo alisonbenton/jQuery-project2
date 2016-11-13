fd4ea5d5f87aaf0fb0fac3c8b7cd10cf6ae18479
alisonbenton <alisonbenton@Alisons-MacBook-Air.local>
2016-11-13 14:28:51 -0700

    All working
    homework

---
+++ main.js
@@ -0,0 +1,66 @@
+/*
+=======================================================
+
+** Week 4 - Project 2**
+*** jQuery Mania ***
+
+
+=======================================================
+*/
+
+// Add your JS here.
+
+
+$(document).ready(function() {
+
+
+//1.
+$(".alertme").click(function(){
+    window.alert("jQuery seems easy so far!");
+  });
+
+//2.
+$(".clickme").click(function(){
+  $(".clickme").html("I was clicked");
+});
+
+//3.
+$(".addStyle").click(function(){
+  $("button").addClass("buttonStyle");
+})
+
+//4.
+$(".addDifferentStyle").click(function(){
+$(".addDifferentStyle").addClass("addUniqueStyle");
+})
+
+//5.
+$(".removeBtn").click(function(){
+  $("button").removeClass("buttonStyle");
+});
+
+//6.
+$(".clickToggle").click(function(){
+  $(".clickToggle").toggleClass("buttonStyle");
+   });
+
+//7.
+$(".squareBtn").click(function(){
+  $(".square").hide(1000);
+})
+
+
+//8.
+$(".circle").click(function(){
+  $(".circle").hide(1000);
+})
+
+
+//9.
+$(".toggleRedSquare").click(function(){
+  $(".redSquare").toggle(1500);
+});
+
+
+
+});  // Close: $(document).ready(function() {
